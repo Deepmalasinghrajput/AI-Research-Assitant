021ca5a3223700d06d3a66e436334591f52cc991
Deepmalasinghrajput <deepmalasingh623@gmail.com>
2026-04-19 17:57:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 from pipeline import run_research_pipeline
+import os
 
 app = Flask(__name__)
 
@@ -22,4 +23,5 @@ def ask():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
